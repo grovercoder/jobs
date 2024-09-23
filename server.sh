3c38810a0fb6c48923ff4163e9c0185b7bc63ac7
Shawn <shawn@shawngrover.ca>
2024-09-23 17:59:02 -0600

    attempting to ensure the virtual environment is active

--- server.sh
+++ server.sh
@@ -1,3 +1,13 @@
 #/bin/bash
 
+# determine script directory
+SCRIPT_DIR=$(dirname "$0")
+
+# ensure we are in that directory
+cd $SCRIPT_DIR
+
+# activate the python environment
+source .venv/bin/activate
+
+# launch the server
 gunicorn --config gunicorn_config.py jobs.server:WEB_APP
